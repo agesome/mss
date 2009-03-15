1d838dc3acc5fcdea2c9d880f4a340b250cef37e
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-03-15 12:46:06 +0200

    Minor cleanup

--- ds18x20.c
+++ ds18x20.c
@@ -21,11 +21,6 @@ changelog:
 #include "onewire.h"
 #include "crc8.h"
 
-//#ifdef DS18X20_EEPROMSUPPORT
-// for 10ms delay in copy scratchpad
-//#include "delay.h"
-//#endif
-
 /* 
    convert raw value from DS18x20 to Celsius
    input is: 
@@ -155,29 +150,11 @@ uint8_t DS18X20_start_meas( uint8_t with_power_extern, uint8_t id[])
 	}
 }
 
-/* reads temperature (scratchpad) of sensor with rom-code id
-   output: subzero==1 if temp.<0, cel: full celsius, mcel: frac 
-   in millicelsius*0.1
-   i.e.: subzero=1, cel=18, millicel=5000 = -18,5000°C */
-uint8_t DS18X20_read_meas(uint8_t id[], uint8_t *subzero, 
-	uint8_t *cel, uint8_t *cel_frac_bits)
-{
-	uint8_t i;
-	uint8_t sp[DS18X20_SP_SIZE];
-	
-	ow_reset(); //**
-	ow_command(DS18X20_READ, id);
-	for ( i=0 ; i< DS18X20_SP_SIZE; i++ ) sp[i]=ow_byte_rd();
-	if ( crc8( &sp[0], DS18X20_SP_SIZE ) ) 
-		return DS18X20_ERROR_CRC;
-	DS18X20_meas_to_cel(id[0], sp, subzero, cel, cel_frac_bits);
-	return DS18X20_OK;
-}
-
 /* reads temperature (scratchpad) of a single sensor (uses skip-rom)
    output: subzero==1 if temp.<0, cel: full celsius, mcel: frac 
    in millicelsius*0.1
    i.e.: subzero=1, cel=18, millicel=5000 = -18,5000°C */
+
 uint8_t DS18X20_read_meas_single(uint8_t familycode, uint8_t *subzero, 
 	uint8_t *cel, uint8_t *cel_frac_bits)
 {
