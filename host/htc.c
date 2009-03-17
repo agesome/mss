84add932dce8542af05d7dad7b5a581d7a3bf257
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-03-17 21:38:19 +0200

    htc now respects window size

--- host/htc.c
+++ host/htc.c
@@ -4,39 +4,48 @@
 #include <iface.h>
 #include <htc.h>
 #include <time.h>
+#include <math.h>
 #define MAXTS 3
 #define MAXHS 2
 
 const char welc[] = "HT217 interface";
 extern char *recvBuffer;
 double tData[MAXTS], hData[MAXHS];
-WINDOW *msgs;
+WINDOW *msgs, *data;
 
 void
-parseRecv(void){
-  sscanf(recvBuffer, "%2.1f %2.1f %2.1f %2.1f %2.1f", &tData[0], &tData[1], &tData[2], &hData[0], &hData[1]);
+parseRecv(char *buf){
+  sscanf(buf, "%2.1f %2.1f %2.1f %2.1f %2.1f", &tData[0], &tData[1], &tData[2], &hData[0], &hData[1]);
 }
 
+void
+updateData(void){
+  wclear(data);
+  box(data, 0, 0);
+  mvwprintw(data, 0, 3, "Data");
+  mvwprintw(data, trunc(LINES / 6 / 2), 2, "T 1: %2.1f; T 2: %2.1f; T 3: %2.1f; Fi 1: %2.1f; Fi 2: %2.1f;", tData[0], tData[1], tData[2], hData[0], hData[1]);
+  wrefresh(data);
+}
 
 /*print str in statustext box*/
 void sText(const char *str){
   int y, ycmp, x;
-  getyx(msgs, y, x);
   
-  if(strlen(str) + x > COLS - 2)
+  /*border checks*/
+  getyx(msgs, y, x);
+  if(strlen(str) + x >= COLS - 1)
     wmove(msgs, ++y, 2);
-  if(y >= 9){
+  if(y >= trunc(LINES / 4) - 2){
     scroll(msgs);
     wmove(msgs, --y, 2);
   }
-  
   wprintw(msgs, str);
-  
+  /* if there was a newline, go to a free line*/
   getyx(msgs, ycmp, x);
   if(y < ycmp)
     wmove(msgs, ycmp, 2);
-  box(msgs, 0, 0);
   getyx(msgs, y, x);
+  box(msgs, 0, 0);
   mvwprintw(msgs, 0, 3, "Status");
   wmove(msgs, y, x);
   wrefresh(msgs);
@@ -52,15 +61,19 @@ void init(void){
   refresh();
   
   /*message box part*/
-  msgs = newwin(10, COLS, LINES - 10, 0);
-  //keypad(msgs, TRUE);
+  msgs = newwin(trunc(LINES / 4), COLS, trunc(LINES - LINES / 4), 0);
   scrollok(msgs, TRUE);
   idlok(msgs, TRUE);
   clearok(msgs, TRUE);
-  wsetscrreg(msgs, 1, 8); 
+  wsetscrreg(msgs, 1, trunc(LINES / 4) - 2); 
   box(msgs, 0, 0);
   wmove(msgs, 1, 2);
   wrefresh(msgs);
+
+  /* data box */
+  data = newwin(trunc(LINES / 5), COLS, trunc(LINES / 5), 0);
+  box(data, 0, 0);
+  wrefresh(data);
   
   /*welcome message*/
   sText("This is HT217 interface version " VERSION ". Press F1 for help.\n");
@@ -70,19 +83,27 @@ void init(void){
 int
 main(int argc, char *argv[]){
   int btn;
-  clock_t execT;
-
+  clock_t execT, delta;
+  char tmp[256];
+  execT = clock() / CLOCKS_PER_SEC;
   init();
-  usbInit();
+  // usbInit();
+  //sText(usbData());
 
   while(1){
+    //updateData();
+    /* if((delta = clock() / CLOCKS_PER_SEC) - execT >= 1){
+      execT = clock() / CLOCKS_PER_SEC;
+      sprintf(tmp, "%d", execT);
+      sText(tmp);
+      }*/
     refresh();
     btn = getch();
     if(btn == 'q')
       break;
     else if(btn == KEY_F(1))
-      sText(helpText);
-  }
+    sText(helpText);
+      }
   
   endwin();
   return 0;
