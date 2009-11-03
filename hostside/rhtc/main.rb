d69cf00b6057e19e1ffad65988108063365410e0
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-11-03 22:15:35 +0200

    Notify with statusbar instead of stdio.

--- hostside/rhtc/main.rb
+++ hostside/rhtc/main.rb
@@ -9,16 +9,16 @@ require 'gui'
 class MainGui
   WTITLE='rHTC'
   MAXTEMP = 125.0
-  DEBUG = 0
-  
+  DEBUG = 3
+
   def initialize
     init_base
     init_menu
     init_statusbar
     init_data
-    
+
     begin
-      @dg = DataGetter.new(500)
+      @dg = DataGetter.new(500, @sb)
     rescue StandardError => why
       puts(why.to_s)
       Kernel::exit
@@ -44,7 +44,7 @@ class MainGui
     @wmain = Gtk::Window.new
     @wmain.set_title WTITLE
     @wmain.signal_connect("delete-event") { exit }
-    
+
     @box_main = Gtk::VBox.new
     @boxes = {
       :menu => Gtk::HBox.new,
@@ -53,12 +53,12 @@ class MainGui
       :controls => Gtk::HBox.new,
       :status => Gtk::HBox.new
     }
-    
+
     @wmain.add @box_main
     @boxes.values.each { |box| @box_main.pack_start(box) }
 
   end
-  
+
   def init_menu
     @menu = MenuBar.new
     @menu.add_item("Main")
@@ -66,11 +66,12 @@ class MainGui
     @menu.add_to(@boxes[:menu])
   end
 
-  def init_statusbar    
+  def init_statusbar
     @statusbar = Gtk::Statusbar.new
     @boxes[:status].add(@statusbar)
+    @sb = StatusBar.new(@statusbar)
   end
-  
+
   def init_data
     @pbar = Gtk::ProgressBar.new
     @boxes[:data].add(@pbar)
@@ -81,44 +82,59 @@ class MainGui
       true
     end
   end
-  
+
+  class StatusBar
+
+    def initialize(instance)
+      @sb = instance
+      @ci = @sb.get_context_id("Status reports")
+    end
+
+    def message(text)
+      @sb.push(@ci, text)
+    end
+
+  end
+
   class DataGetter
-    
-    def initialize(delay)
+
+    def initialize(delay, statusbar)
       raise "delay must be Fixnum" unless delay.kind_of? Fixnum
       connect(delay)
+      @sb = statusbar
     end
-    
+
     def do_fetch(delay)
       GLib::Timeout.add(delay) do
         begin
           m = @if.fetch
-          
+
         rescue EOFError => why
-          puts "Exiting: #{why.to_s}"
+          puts("Exiting: #{why.to_s}")
           exit
         rescue StandardError => why
-          puts "Warning: #{why.to_s}"
+          @sb.message("Warning: #{why.to_s}")
+          retry
         else
           @data = m
         end
         @fetch
       end
     end
-    
+
     def temp
       return @data[0]
     end
-    
+
     def uptime
       return @data[1]
     end
-    
+
     def disconnect
       @fetch = false
       @if.destroy
     end
-    
+
     def connect(delay)
       raise "delay must be Fixnum" unless delay.kind_of? Fixnum
       begin
