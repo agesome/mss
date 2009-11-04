68afc71f00c4db6e8674cfcdf924fd6a90587562
Evgeny Grablyk <evgeny.grablyk@gmail.com>
2009-11-04 22:40:12 +0200

    Started implementing logging and charting.

--- hostside/rhtc/graph.rb
+++ hostside/rhtc/graph.rb
@@ -1,21 +1,44 @@
-class BlockGraph
-  # cairo context, current drawing coolor
-  attr_accessor :context, :lwidth
+class Graph
+  attr_accessor :cr
 
-  def initialize xsize, ysize
-    @zline = ysize / 2
-    @cx = 0
+  def initialize(points)
+    @points = points
   end
 
-  def draw_stick h, w
-    context.set_line_width lwidth
-    context.set_source_rgb 0.45, 0.823, 0.086
-    context.rectangle @cx, @zline, w, -h
-    context.stroke 1
-    context.rectangle @cx + lwidth, @zline - lwidth, w - lwidth, -h + lwidth
-    context.set_source_rgb 0.3, 0.6, 0.02
-    context.fill
-    context.stroke
-    @cx += w
+  def curve
+    prepared_data = []
+    for i in 0...@points.size
+      x, y = @points[i][0], @points[i][1]
+      
+      if i != 0 and i != @points.size - 1
+        x_left, y_left = @points[i - 1][0], @points[i - 1][1]
+        x_right, y_right = @points[i + 1][0], @points[i + 1][1]
+        step_x_left = (x - x_left) / 2
+        step_x_right = (x_right - x) / 2
+        dx, dy = x_right - x_left, y_right - y_left
+        h = Math.sqrt(dx**2 + dy**2)
+        if h == 0
+          cx1, cy1, cx2, cy2 = x, y, x, y
+        else
+          dx1, dy1 = (dx * step_x_left) / h, (dy * step_x_left) / h
+          dx2, dy2 = (dx * step_x_right) / h, (dy * step_x_right) / h
+          cx1, cx2 = x - dx1, x + dx2
+          cy1, cy2 = y - dy1, y + dy2
+        end
+      else
+        cx1, cy1, cx2, cy2 = x, y, x, y
+      end
+      prepared_data << [x, y, cx1, cy1, cx2, cy2]
+    end
+    
+    for i in 0...prepared_data.size - 1 #range(0, len(prepared_data) - 1):
+      x, y = prepared_data[i][0], prepared_data[i][1]
+      cx1, cy1 = prepared_data[i][4], prepared_data[i][5]
+      cx2, cy2 = prepared_data[i + 1][2], prepared_data[i + 1][3]
+      x2, y2 = prepared_data[i + 1][0], prepared_data[i + 1][1]
+      cr.move_to(x, y)
+      cr.curve_to(cx1, cy1, cx2, cy2, x2, y2)
+      cr.stroke
+    end
   end
 end
--- hostside/rhtc/main.rb
+++ hostside/rhtc/main.rb
@@ -9,20 +9,21 @@ require 'gui'
 class MainGui
   WTITLE='rHTC'
   MAXTEMP = 125.0
-  DEBUG = 3
 
   def initialize
-    init_base
-    init_menu
-    init_statusbar
-    init_data
-
     begin
-      @dg = DataGetter.new(500, @sb)
+      @dg = DataGetter.new(1000, @sb)
     rescue StandardError => why
       puts(why.to_s)
       Kernel::exit
     end
+    
+    init_base
+    init_menu
+    init_log
+    init_statusbar
+    init_data
+    init_graphs
   end
 
   public
@@ -37,7 +38,7 @@ class MainGui
     @dg.disconnect
     Gtk.main_quit
   end
-
+  
   private
 
   def init_base
@@ -49,12 +50,11 @@ class MainGui
     @boxes = {
       :menu => Gtk::HBox.new,
       :data => Gtk::HBox.new,
-      :graphs => Gtk::VBox.new,
+      :graphs => Gtk::HBox.new,
       :controls => Gtk::HBox.new,
       :status => Gtk::HBox.new
     }
-
-    @wmain.add @box_main
+    @wmain.add(@box_main)
     @boxes.values.each { |box| @box_main.pack_start(box) }
 
   end
@@ -63,13 +63,20 @@ class MainGui
     @menu = MenuBar.new
     @menu.add_item("Main")
     @menu.add_subitem("Main", "Quit") { exit }
+
+    @menu.add_subitem("Main", "Log") { @logwindow.vtoggle }
+
     @menu.add_to(@boxes[:menu])
   end
 
   def init_statusbar
     @statusbar = Gtk::Statusbar.new
     @boxes[:status].add(@statusbar)
-    @sb = StatusBar.new(@statusbar)
+    @sb = StatusBar.new(@statusbar, @logwindow)
+  end
+
+  def init_log
+  @logwindow = LogWindow.new(@wmain)
   end
 
   def init_data
@@ -83,69 +90,123 @@ class MainGui
     end
   end
 
-  class StatusBar
 
-    def initialize(instance)
-      @sb = instance
-      @ci = @sb.get_context_id("Status reports")
-    end
-
-    def message(text)
-      @sb.push(@ci, text)
-    end
-
+  def init_graphs
+    @tgraph = Graph.new(@dg.points)
+    da = Gtk::DrawingArea.new
+    da.set_size_request(300, 200)
+    @boxes[:graphs].pack_start(da)
+    da.signal_connect("expose-event") {
+      @tgraph.cr = da.window.create_cairo_context
+      @tgraph.curve
+    }
   end
 
-  class DataGetter
+end
 
-    def initialize(delay, statusbar)
-      raise "delay must be Fixnum" unless delay.kind_of? Fixnum
-      connect(delay)
-      @sb = statusbar
-    end
+# form a log of statusbar messages
+class LogWindow
 
-    def do_fetch(delay)
-      GLib::Timeout.add(delay) do
-        begin
-          m = @if.fetch
+  def initialize(window)
+    @logw = Gtk::Window.new("Events log")
+    @buffer = Gtk::TextBuffer.new
+    @view = Gtk::TextView.new
+    @view.buffer = @buffer
+    @logw.add(@view)
+  end
 
-        rescue EOFError => why
-          puts("Exiting: #{why.to_s}")
-          exit
-        rescue StandardError => why
-          @sb.message("Warning: #{why.to_s}")
-          retry
-        else
-          @data = m
-        end
-        @fetch
-      end
-    end
+  def add_msg(text)
+    @buffer.insert_at_cursor(text + "\n")
+  end
 
-    def temp
-      return @data[0]
-    end
+  def show
+    @logw.show_all
+  end
 
-    def uptime
-      return @data[1]
-    end
+  def hide
+    @logw.hide_all
+  end
 
-    def disconnect
-      @fetch = false
-      @if.destroy
-    end
+  def vtoggle
+    if @logw.visible? then hide else show end
+  end
 
-    def connect(delay)
-      raise "delay must be Fixnum" unless delay.kind_of? Fixnum
+end
+
+class StatusBar
+
+  def initialize(instance, logwindow)
+    @sb = instance
+    @ci = @sb.get_context_id("Status reports")
+    @lw = logwindow
+  end
+
+  def message(text)
+    @sb.pop(@ci)
+    msg = Time.now.strftime("%H:%M:%S") + ': ' + text
+    @sb.push(@ci, msg)
+    @lw.add_msg(msg)
+  end
+
+end
+
+class DataGetter
+  DEBUG = 3
+
+  def initialize(delay, statusbar)
+    raise "delay must be Fixnum" unless delay.kind_of? Fixnum
+    connect(delay)
+    @sb = statusbar
+    @points = []
+  end
+
+  def do_fetch(delay)
+    iteration = 0
+    GLib::Timeout.add(delay) do
       begin
-        @if = HTUSBInterface.new(DEBUG)
+        m = @if.fetch
+      rescue EOFError => why
+#        puts("Exiting: #{why.to_s}")
+        exit
       rescue StandardError => why
-        raise StandardError, "Failed to initialize USB interface: #{why.to_s}"
+#        @sb.message("Warning: #{why.to_s}")
+        retry
+      else
+        @data = m
+        iteration += 1
+        @points << [iteration * 5, (m[0]/10).to_i]#((150 / @data[0]) * 100).to_i]
       end
-      @fetch = true
-      @data = [0, 0]
-      do_fetch(delay)
+      @fetch
     end
+  end
+
+  def temp
+    return @data[0]
+  end
+
+  def uptime
+    return @data[1]
+  end
+
+  def points
+    return @points
+  end
+
+  def disconnect
+    @fetch = false
+    @if.destroy
+  end
+
+  def connect(delay)
+    raise "delay must be Fixnum" unless delay.kind_of? Fixnum
+    begin
+      @if = HTUSBInterface.new(DEBUG)
+    rescue StandardError => why
+      raise StandardError, "Failed to initialize USB interface: #{why.to_s}"
+    end
+    @fetch = true
+    @data = [0, 0]
+    do_fetch(delay)
   end
 end
 
